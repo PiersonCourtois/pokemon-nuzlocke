31ebb38cfe6c71fe3ef31de5137dceddaf7f921d
Pierson Courtois <courtoispierson@gmail.com>
2023-01-04 02:25:09 -0600

    ga4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Run from './components/Run';
+import ReactGA from "react-ga4";
 import Levels from './components/Levels';
 import { userData } from "./user-data";
 import './styles.css';
 
 const App = () => {
+    ReactGA.initialize("G-8SCM7E14SG");
+    ReactGA.send("pageview");
     // Initializes userData if first time entering page
     if (localStorage.getItem('userData') === null) {
         localStorage.setItem('userData', JSON.stringify(userData));
